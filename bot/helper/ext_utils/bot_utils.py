e90c86d4ca91f95a3ebf75bd86a569079d7cbb60
ProXharsh <130951750+ProXharsh@users.noreply.github.com>
2023-07-23 21:20:38 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -123,7 +123,7 @@ def get_progress_bar_string(status):
 
 def get_readable_message():
     with download_dict_lock:
-        msg = f"<b>● Pᴏᴡᴇʀᴇᴅ Bʏ 𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭</b>\n\n"
+        msg = f"<b>● Pᴏᴡᴇʀᴇᴅ Bʏ ♡ 𝐏𝐑𝐎𝐅𝐅𝐄𝐒𝐎𝐑 ♡</b>\n\n"
         if STATUS_LIMIT is not None:
             tasks = len(download_dict)
             global pages
@@ -159,7 +159,7 @@ def get_readable_message():
             else:
                 msg += ''
             msg += f"\n<code>/{BotCommands.CancelMirror} {download.gid()}</code>"
-            msg += f"\n<b>⊱✤┅●( 𝐀ɴɢᴇʟ✘𝐎ᴘ 𝐋𝐨𝐋 )●┅✤⊰</b>"
+            msg += f"\n<b>⊱✤┅●( ♡ ᗰᗩ᙭ ᒪᗴᗴᑕᕼ 乙ᗝᑎᗴ ♡ )●┅✤⊰</b>"
             msg += "\n\n"
             if STATUS_LIMIT is not None and index == STATUS_LIMIT:
                 break
@@ -186,12 +186,12 @@ def get_readable_message():
                     up_speed += float(spd.split('K')[0]) * 1024
                 elif 'M' in spd:
                     up_speed += float(spd.split('M')[0]) * 1048576
-        bmsg = f"\n🌶 𝐀ɴɢᴇʟ✘𝐎ᴘ 𝐋𝐨𝐋 🌶"            
+        bmsg = f"\n★彡 𝐌𝐀𝐗 𝐋𝐄𝐄𝐂𝐇 𝐙𝐎𝐍𝐄 彡★"            
         bmsg += f"\n<b>◉ Cᴘᴜ:</b> {cpu_percent()}% | <b>◉ Fʀᴇᴇ:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
         bmsg += f"\n<b>◉ Rᴀᴍ:</b> {virtual_memory().percent}% | <b>◉ Uᴘᴛɪᴍᴇ:</b> {get_readable_time(time() - botStartTime)}"
         bmsg += f"\n<b>⇓ Dʟ:</b> {get_readable_file_size(dl_speed)}/s | <b>⇑ Uʟ:</b> {get_readable_file_size(up_speed)}/s"
         buttons = ButtonMaker()
-        buttons.sbutton("🦋 𝐀ɴɢᴇʟ✘𝐎ᴘ 𝐋𝐨𝐋 🦋", str(FOUR))
+        buttons.sbutton("🕊️ 𝐌𝐀𝐗 𝐋𝐄𝐄𝐂𝐇 𝐙𝐎𝐍𝐄 🕊️", str(FOUR))
         sbutton = buttons.build_menu(1)
         if STATUS_LIMIT is not None and tasks > STATUS_LIMIT:
             msg += f"<b>◉ Page:</b> {PAGE_NO}/{pages} | <b>◉ Tasks:</b> {tasks}\n"
@@ -347,7 +347,7 @@ DL ● {num_active} | UP ● {num_upload}
 SPLIT ● {num_split}
 ZIP ● {num_archi} | UNZIP ● {num_extract}
 TOTAL ● {tasks} 
-● Powered By ● ❤️ 𝐖𝐃 𝐙𝐎𝐍𝐄
+● Powered By ● ❤️ ♡ 𝐏𝐑𝐎𝐅𝐅𝐄𝐒𝐎𝐑 ♡
 """
     return stats
 dispatcher.add_handler(
