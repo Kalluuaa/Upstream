283696f2446c9b26770ec8dea3c6bdc02a48b209
ProXharsh <130951750+ProXharsh@users.noreply.github.com>
2023-07-23 21:13:37 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -72,7 +72,7 @@ class TelegraphHelper:
                     nxt_page += 1
             self.edit_page(
                 path = path[prev_page],
-                title = 'WD Zone Mirror Torrent Search',
+                title = 'Max Leech Zone Torrent Search',
                 content=content
             )
         return
